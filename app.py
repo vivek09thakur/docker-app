5dafe84c29b1503eb7642686bdaeaf4b6cfcdf5e
Vivek Thakur <vivekthakurcse20509@gmail.com>
2026-02-23 06:23:26 +0000

    commit

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/getitems")
+async def getitem():
+    return {
+        "item-list":
+        {
+            "1": "patato",
+            "2": "tomato"
+        }
+    }
